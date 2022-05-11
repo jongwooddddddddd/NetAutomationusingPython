6826906546922283c04eaed4514a8696bd49b328
jongwoo0270@naver.com <jongwoo0270@naver.com>
2022-05-11 13:35:44 +0900

    aaa

---
+++ aaa.py
@@ -0,0 +1,4 @@
+print "Python Programming Test in Nexus Switch1"
+print "Python Programming Test in Nexus Switch2"
+print "Python Programming Test in Nexus Switch3"
+print "Python Programming Test in Nexus Switch4"
